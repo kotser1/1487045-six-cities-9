e5f4c6350f3eaab99e031ff1598996e83808f0a5
kotser1 <newelvin@gmail.com>
2022-02-21 15:43:00 +0300

    Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+
 import MainPage from '../main-page/main-page';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import Property from '../property/property';
+import Page404 from '../page404/page404';
+import PrivateRoute from '../private-route/private-route';
+
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 type AppProps = {
   offersCount: number;
@@ -7,7 +16,35 @@ type AppProps = {
 
 function App({offersCount, displayedOffersCount}: AppProps): JSX.Element {
   return (
-    <MainPage offersCount={offersCount} displayedOffersCount={displayedOffersCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage offersCount={offersCount} displayedOffersCount={displayedOffersCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Property}
+          element={<Property />}
+        />
+        <Route
+          path='*'
+          element={<Page404 />}
+        />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
