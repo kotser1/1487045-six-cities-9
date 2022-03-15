cb204c1b3b934257133f4a3b3ee2ddfc5dc3698d
kotser1 <newelvin@gmail.com>
2022-03-15 14:31:03 +0300

    Добавляет компонент Header

--- project/src/pages/page404/page404.tsx
+++ project/src/pages/page404/page404.tsx
@@ -1,3 +1,4 @@
+import Header from '../../components/header/header';
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
 
@@ -6,39 +7,7 @@ import './page404.css';
 function Page404() {
   return (
     <div className="page page--not-found">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link className="header__logo-link" to={AppRoute.Root}>
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width="81"
-                  height="41"
-                />
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="/#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main className="page__main page__main--not-found">
         <div className="not-found-container container">
