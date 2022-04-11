3b5b69fddc6d36ad10a6f38f8f0b101a104f8350
kotser1 <newelvin@gmail.com>
2022-04-11 10:01:43 +0300

    Добавляет переадресацию со страницы /login если авторизован

--- project/src/pages/login/login.tsx
+++ project/src/pages/login/login.tsx
@@ -1,14 +1,17 @@
 import { useRef, FormEvent } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
-import { useAppDispatch } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import { loginAction } from '../../store/api-actions';
 import { AuthData } from '../../types/auth-data';
 import { AppRoute } from '../../const';
+import { isAuth } from '../../utils';
 
 function Login(): JSX.Element {
   const loginRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const authorizationStatus = useAppSelector(({USER}) => USER.authorizationStatus);
 
   const dispatch = useAppDispatch();
 
@@ -40,55 +43,58 @@ function Login(): JSX.Element {
           </div>
         </div>
       </header>
-
-      <main className="page__main page__main--login">
-        <div className="page__login-container container">
-          <section className="login">
-            <h1 className="login__title">Sign in</h1>
-            <form
-              className="login__form form"
-              action="#"
-              method="post"
-              onSubmit={handleSubmit}
-            >
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">E-mail</label>
-                <input
-                  ref={loginRef}
-                  className="login__input form__input"
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  required
-                  pattern='^[a-zA-z0-9\.]+@[a-zA-Z0-9-]+\.[a-zA-Z]+'
-                  title='Invalid email!'
-                />
-              </div>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">Password</label>
-                <input
-                  ref={passwordRef}
-                  className="login__input form__input"
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  required
-                  pattern='^[^\s]+$'
-                  title='Password must not contain spaces!'
-                />
-              </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
-            </form>
-          </section>
-          <section className="locations locations--login locations--current">
-            <div className="locations__item">
-              <a className="locations__item-link" href="/#">
-                <span>Amsterdam</span>
-              </a>
+      {isAuth(authorizationStatus)
+        ? <Navigate to={AppRoute.Root} />
+        : (
+          <main className="page__main page__main--login">
+            <div className="page__login-container container">
+              <section className="login">
+                <h1 className="login__title">Sign in</h1>
+                <form
+                  className="login__form form"
+                  action="#"
+                  method="post"
+                  onSubmit={handleSubmit}
+                >
+                  <div className="login__input-wrapper form__input-wrapper">
+                    <label className="visually-hidden">E-mail</label>
+                    <input
+                      ref={loginRef}
+                      className="login__input form__input"
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      required
+                      pattern='^[a-zA-z0-9\.]+@[a-zA-Z0-9-]+\.[a-zA-Z]+'
+                      title='Invalid email!'
+                    />
+                  </div>
+                  <div className="login__input-wrapper form__input-wrapper">
+                    <label className="visually-hidden">Password</label>
+                    <input
+                      ref={passwordRef}
+                      className="login__input form__input"
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      required
+                      pattern='^[^\s]+$'
+                      title='Password must not contain spaces!'
+                    />
+                  </div>
+                  <button className="login__submit form__submit button" type="submit">Sign in</button>
+                </form>
+              </section>
+              <section className="locations locations--login locations--current">
+                <div className="locations__item">
+                  <a className="locations__item-link" href="/#">
+                    <span>Amsterdam</span>
+                  </a>
+                </div>
+              </section>
             </div>
-          </section>
-        </div>
-      </main>
+          </main>
+        )}
     </div>
   );
 }
